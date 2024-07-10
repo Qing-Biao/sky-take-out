d006295c80e82478d3bd3c83dd2d664343197793
xie <2821293903@qq.com>
2024-07-10 16:07:42 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,12 +1,16 @@
 package com.sky.controller.user;
 
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("userShopController")
 @RequestMapping("/user/shop")
